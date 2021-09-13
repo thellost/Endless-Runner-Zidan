aeb05b1edd20bb971e1623b0d95481b53a60d9d0
thellost <aldy10ball@gmail.com>
2021-09-13 22:53:23 +0700

    Commit base project

---
+++ Endless Runner Project/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+
+
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 velocityVector = rig.velocity;
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+}
